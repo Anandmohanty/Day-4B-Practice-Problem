9dde3b2cab46b61529c61341d0fe23cb0f3c44f9
Anandmohanty <mohantyanand3@gmail.com>
2023-04-18 23:38:49 +0530

    [Anand] - Welcome 4B-PracticeProblem

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Day_4B_Practice_Problem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Day-4B-Practice Problem");
+        }
+    }
+}
